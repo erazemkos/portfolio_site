69bfe07813bd5540ca72441cf4d5e25246775b7c
erazemkos <erazem.kos@hotmail.com>
2024-02-12 21:48:56 +0100

    changed look, added a new page

--- static/js/src.js
+++ static/js/src.js
@@ -1,9 +1,65 @@
+/* -- Carousel Navigation -- */
+
+let activeIndex = 0;
+
+const slides = document.getElementsByTagName("article");
+
+const handleLeftClick = () => {
+  const nextIndex = activeIndex - 1 >= 0 ? activeIndex - 1 : slides.length - 1;
+  
+  const currentSlide = document.querySelector(`[data-index="${activeIndex}"]`),
+        nextSlide = document.querySelector(`[data-index="${nextIndex}"]`);
+        
+  currentSlide.dataset.status = "after";
+  
+  nextSlide.dataset.status = "becoming-active-from-before";
+  
+  setTimeout(() => {
+    nextSlide.dataset.status = "active";
+    activeIndex = nextIndex;
+  });
+}
+
+const handleRightClick = () => {
+  const nextIndex = activeIndex + 1 <= slides.length - 1 ? activeIndex + 1 : 0;
+  
+  const currentSlide = document.querySelector(`[data-index="${activeIndex}"]`),
+        nextSlide = document.querySelector(`[data-index="${nextIndex}"]`);
+  
+  currentSlide.dataset.status = "before";
+  
+  nextSlide.dataset.status = "becoming-active-from-after";
+  
+  setTimeout(() => {
+    nextSlide.dataset.status = "active";
+    activeIndex = nextIndex;
+  });
+}
+
+/* -- Mobile Nav Toggle -- */
+
+const nav = document.querySelector("nav");
+
+const handleNavToggle = () => {  
+  nav.dataset.transitionable = "true";
+  
+  nav.dataset.toggled = nav.dataset.toggled === "true" ? "false" : "true";
+}
+
+window.matchMedia("(max-width: 800px)").onchange = e => {
+  nav.dataset.transitionable = "false";
+
+  nav.dataset.toggled = "false";
+};
+
+/* -- Scramble Text Effect -- */
+
 const toggleNav = () => {
   document.body.dataset.nav = document.body.dataset.nav === "true" ? "false" : "true";
 }
 
 document.addEventListener("DOMContentLoaded", function() {
-    const elements = document.querySelectorAll("h1, p");
+    const elements = document.querySelectorAll("h1, p, h2");
 
     elements.forEach(element => {
         function scrambleText() {
@@ -41,5 +97,3 @@ document.addEventListener("DOMContentLoaded", function() {
         console.log("No h1 or p elements found");
     }
 });
-
-
